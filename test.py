89609a70ac88c739d7d51f9390f0c83d7e6b9524
conner yule <conneryule@gmail.com>
2025-08-12 23:37:44 -0400

    working user data preprocess

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-import train_model
+import data_preprocessing
+from user_features import UserFeatures
 
-print("Log")
-train_model.logistic_model()
-print()
-print("Rand forest")
-train_model.random_Forest()
+
+def test_data_preprocess():
+    data_preprocessing.preprocess_data()
+
+
+def test_user_data_prec():
+    user_data = UserFeatures(
+        user_time_alone=5.0,
+        user_stage_fear="Yes",
+        user_social_event=3.0,
+        user_time_outside=5.0,
+        user_drained_event="Yes",
+        user_friend_num=2.0,
+        user_post_num=3.0,
+    )
+
+    test = data_preprocessing.preprocess_user_data(user_data)
+    print(test)
+
+
+if __name__ == "__main__":
+    test_data_preprocess()
+    test_user_data_prec()
--- user_features.py
+++ user_features.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel
 # Need to fix these types, idk what they are just yet
 class UserFeatures(BaseModel):
     user_time_alone: float
-    user_stage_fear: float
+    user_stage_fear: str
     user_social_event: float
     user_time_outside: float
-    user_drained_event: float
-    user_friend_num: int
-    user_post_num: int
+    user_drained_event: str
+    user_friend_num: float
+    user_post_num: float
